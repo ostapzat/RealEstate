8f91866132a613543483834eecaca0df2d84d4d5
Ostap <ostzat@gmail.com>
2023-06-03 20:31:30 +0200

    added adaption (not end)

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,8 @@ const menu = document.querySelector(".menu");
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("menu-active");
 });
+function toggleAccordion(element) {
+  var accordionItem = element.parentNode;
+  var accordionContent = accordionItem.querySelector(".accordion-content");
+  accordionContent.classList.toggle("active");
+}
