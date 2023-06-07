26ada242e169053cf5fa6768e0e4b54a01e0fa70
Ostap <ostzat@gmail.com>
2023-06-07 09:49:15 +0200

    added living section with adaption

--- src/js/script.js
+++ src/js/script.js
@@ -10,21 +10,3 @@ function toggleAccordion(element) {
   accordionContent.classList.toggle("active");
 }
 
-// Вимкнути горизонтальну прокрутку
-function disableHorizontalScroll() {
-  // Запам'ятати поточне положення прокрутки
-  var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-
-  // Встановити стиль body, щоб блокувати горизонтальну прокрутку
-  document.body.style.overflowX = "hidden";
-
-  // Встановити положення прокрутки на початкове значення
-  window.scrollTo(scrollLeft, 0);
-}
-
-// Включити горизонтальну прокрутку
-function enableHorizontalScroll() {
-  // Відновити стиль body для дозволу горизонтальної прокрутки
-  document.body.style.overflowX = "auto";
-}
-enableHorizontalScroll();
